5d79228ef4a6338000eaccd37cf76be7a009a2c5
Caique Rodrigues <caiquedev.estudos@gmail.com>
2024-10-27 22:14:13 -0300

    feat: adicionando o projeto atual para ser trabalhado em sala de aula

---
+++ src/migrations/migration.ts
@@ -0,0 +1,24 @@
+
+import pool from '../config/database';
+
+const createBooksTable = async () => {
+  const client = await pool.connect();
+  try {
+    const queryText = `
+      CREATE TABLE IF NOT EXISTS books (
+        isbn13 SERIAL PRIMARY KEY,
+        titlel VARCHAR(100) NOT NULL,
+        subtitle VARCHAR(100) NOT NULL,
+        price NUMERIC(10,2) NOT NULL
+      );
+    `;
+    await client.query(queryText);
+    console.log('Tabela "books" criada com sucesso!');
+  } catch (err) {
+    console.error('Erro ao criar tabela:', err);
+  } finally {
+    client.release();
+  }
+};
+
+createBooksTable().then(() => process.exit(0));
