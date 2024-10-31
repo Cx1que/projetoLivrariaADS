1132e66d48d6d378d9d958d1d11c4b1f2b3223c2
Caique Rodrigues <caiquedev.estudos@gmail.com>
2024-10-31 01:43:48 -0300

    feat: criação de novas tabelas no banco de dados

--- src/migrations/migration.ts
+++ src/migrations/migration.ts
@@ -5,15 +5,15 @@ const createBooksTable = async () => {
   const client = await pool.connect();
   try {
     const queryText = `
-      CREATE TABLE IF NOT EXISTS books (
-        isbn13 SERIAL PRIMARY KEY,
-        titlel VARCHAR(100) NOT NULL,
+      CREATE TABLE IF NOT EXISTS books_on_the_table (
+        isbn SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
         subtitle VARCHAR(100) NOT NULL,
         price NUMERIC(10,2) NOT NULL
       );
     `;
     await client.query(queryText);
-    console.log('Tabela "books" criada com sucesso!');
+    console.log('Tabela criada com sucesso!');
   } catch (err) {
     console.error('Erro ao criar tabela:', err);
   } finally {
